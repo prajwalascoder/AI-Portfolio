0bed0b2a368912ab2cb632d36b5927df735122a6
Prajwalascoder <prajwalkarthik01@gmail.com>
2026-01-19 00:31:08 +0530

    fixed case sensitive isuue

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import chatRoutes from "./routes/chatroutes.js";
+import chatRoutes from "./routes/chatRoutes.js";
 
 dotenv.config();
 
